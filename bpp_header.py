8df5d9711a38224860e86582feb83706e419874d
viachpaliy <viachpaliy@ukr.net>
2020-09-01 16:22:59 +0300

    add new methods

--- bpp_header.py
+++ bpp_header.py
@@ -5,10 +5,16 @@ class BPP_Header:
         self.ver = ver
 
     def to_bpp_code(self):
-        return '[HEADER]\nTYPE={0}\nVER={1}'.format(self.prg_type,int(self.ver))
+        return '[HEADER]\nTYPE={0}\nVER={1}\n'.format(self.prg_type,int(self.ver))
 
+    def to_cix(self):
+        return 'BEGIN ID CID3\n	REL= 5.0\nEND ID\n'
 
+    def to_cid(self):
+        return 'BEGIN ID CID3\n' +"'" + '"untitled"\n  REL= 3.0\n  AXIS=x+,y-,z-\nEND ID\n'
 
 if __name__=='__main__':
     h = BPP_Header()
     print(h.to_bpp_code())
+    print(h.to_cix())
+    print(h.to_cid())
--- bpp_maindata.py
+++ bpp_maindata.py
@@ -7,7 +7,7 @@ class BPPmaindata:
         self.simmetry = 1
         self.tlchk = 0
         self.tooling = ""
-        self.custstr = '$B$KBsExportToNcRvA.XncExtraPanelData$V""'
+        self.custstr = ""
         self.fcn = 1.0
         self.xcut = 0.0
         self.ycut = 0.0
@@ -33,7 +33,7 @@ class BPPmaindata:
         self.enablefastvertborings = 0
         self.fastvertboringvalue = 0.0
         
-    def to_bpp_code(self):
+    def to_bpp_code150(self):
         string = 'PAN=LPX|{:4.3f}||4|\n'.format(self.lpx) +\
                  'PAN=LPY|{:4.3f}||4|\n'.format(self.lpy) +\
                  'PAN=LPZ|{:3.3f}||4|\n'.format(self.lpz) +\
@@ -41,7 +41,7 @@ class BPPmaindata:
                  'PAN=SIMMETRY|{:d}||1|\n'.format(self.simmetry) +\
                  'PAN=TLCHK|{:d}||1|\n'.format(self.tlchk) +\
                  'PAN=TOOLING|"{}"||3|\n'.format(self.tooling) +\
-                 'PAN=CUSTSTR|{}||3|\n'.format(self.custstr) +\
+                 'PAN=CUSTSTR|$B$KBsExportToNcRvA.XncExtraPanelData$V"{}"||3|\n'.format(self.custstr) +\
                  'PAN=FCN|{:2.6f}||2|\n'.format(self.fcn) +\
                  'PAN=XCUT|{}||4|\n'.format(self.xcut) +\
                  'PAN=YCUT|{}||4|\n'.format(self.ycut) +\
@@ -69,7 +69,109 @@ class BPPmaindata:
 
         return string
 
+    def to_bpp_code130(self):
+        string = 'PAN=LPX|{:4.3f}||4|\n'.format(self.lpx) +\
+                 'PAN=LPY|{:4.3f}||4|\n'.format(self.lpy) +\
+                 'PAN=LPZ|{:3.3f}||4|\n'.format(self.lpz) +\
+                 'PAN=ORLST|"{}"||0|\n'.format(self.orlst) +\
+                 'PAN=SIMMETRY|{:d}||0|\n'.format(self.simmetry) +\
+                 'PAN=TLCHK|{:d}||0|\n'.format(self.tlchk) +\
+                 'PAN=TOOLING|"{}"||0|\n'.format(self.tooling) +\
+                 'PAN=CUSTSTR|"{}"||0|\n'.format(self.custstr) +\
+                 'PAN=FCN|{:2.6f}||0|\n'.format(self.fcn) +\
+                 'PAN=XCUT|{}||4|\n'.format(self.xcut) +\
+                 'PAN=YCUT|{}||4|\n'.format(self.ycut) +\
+                 'PAN=JIGTH|{}||4|\n'.format(self.jigth) +\
+                 'PAN=CKOP|{:d}||0|\n'.format(self.ckop) +\
+                 'PAN=UNIQUE|{:d}||0|\n'.format(self.unique) +\
+                 'PAN=MATERIAL|"{}"||0|\n'.format(self.material) +\
+                 'PAN=PUTLST|"{}"||0|\n'.format(self.putlst) +\
+                 'PAN=OPPWKRS|{:d}||0|\n'.format(self.oppwkrs) +\
+                 'PAN=UNICLAMP|{:d}||0|\n'.format(self.uniclamp) +\
+                 'PAN=CHKCOLL|{:d}||0|\n'.format(self.chkcoll) +\
+                 'PAN=WTPIANI|{:d}||0|\n'.format(self.wtpiani) +\
+                 'PAN=COLLTOOL|{:d}||0|\n'.format(self.colltool) +\
+                 'PAN=CALCEDTH|{:d}||0|\n'.format(self.calcedth) +\
+                 'PAN=ENABLELABEL|{:d}||0|\n'.format(self.enablelabel)
+        return string
+
+    def to_vb_script(self):
+        string = 'Dim LPX: LPX = {:4.3f}\n'.format(self.lpx) +\
+                 'Dim LPY: LPY = {:4.3f}\n'.format(self.lpy) +\
+                 'Dim LPZ: LPZ = {:3.3f}\n'.format(self.lpz) +\
+                 'Dim ORLST: ORLST = "{}"\n'.format(self.orlst) +\
+                 'Dim SIMMETRY: SIMMETRY = {:d}\n'.format(self.simmetry) +\
+                 'Dim TLCHK: TLCHK = {:d}\n'.format(self.tlchk) +\
+                 'Dim TOOLING: TOOLING = "{}"\n'.format(self.tooling) +\
+                 'Dim CUSTSTR: CUSTSTR = "{}"\n'.format(self.custstr) +\
+                 'Dim FCN: FCN = {:2.6f}\n'.format(self.fcn) +\
+                 'Dim XCUT: XCUT = {}\n'.format(self.xcut) +\
+                 'Dim YCUT: YCUT = {}\n'.format(self.ycut) +\
+                 'Dim JIGTH: JIGTH = {}\n'.format(self.jigth) +\
+                 'Dim CKOP: CKOP = {:d}\n'.format(self.ckop) +\
+                 'Dim UNIQUE: UNIQUE = {:d}\n'.format(self.unique) +\
+                 'Dim MATERIAL: MATERIAL = "{}"\n'.format(self.material) +\
+                 'Dim PUTLST: PUTLST = "{}"\n'.format(self.putlst) +\
+                 'Dim OPPWKRS: OPPWKRS = {:d}\n'.format(self.oppwkrs) +\
+                 'Dim UNICLAMP: UNICLAMP = {:d}\n'.format(self.uniclamp) +\
+                 'Dim CHKCOLL: CHKCOLL = {:d}\n'.format(self.chkcoll) +\
+                 'Dim WTPIANI: WTPIANI = {:d}\n'.format(self.wtpiani) +\
+                 'Dim COLLTOOL: COLLTOOL = {:d}\n'.format(self.colltool) +\
+                 'Dim CALCEDTH: CALCEDTH = {:d}\n'.format(self.calcedth) +\
+                 'Dim ENABLELABEL: ENABLELABEL = {:d}\n'.format(self.enablelabel)
+        return string
+
+    def to_cid(self):
+        string ='BEGIN MAINDATA\n  LX={:4.3f}\n'.format(self.lpx) +\
+                '  LY={:4.3f}\n'.format(self.lpy) +\
+                '  LZ={:3.3f}\n'.format(self.lpz) +\
+                '  OX= 0.00\n  OY= 0.00\n  OZ= 0.00\n' +\
+                '\tBEGIN TECH\n    CAT=0\n        END TECH\nEND MAINDATA \n'
+        return string
+
+    def to_cix(self):
+        string = 'BEGIN MAINDATA\n\tLPX={:4.3f}\n'.format(self.lpx) +\
+                 '\tLPY={:4.3f}\n'.format(self.lpy) +\
+                 '\tLPZ={:3.3f}\n'.format(self.lpz) +\
+                 '\tORLST="{}"\n'.format(self.orlst) +\
+                 '\tSIMMETRY={:d}\n'.format(self.simmetry) +\
+                 '\tTLCHK={:d}\n'.format(self.tlchk) +\
+                 '\tTOOLING="{}"\n'.format(self.tooling) +\
+                 '\tCUSTSTR="$B$KBsExportToNcRvA.XncExtraPanelData$V""{}"\n'.format(self.custstr) +\
+                 '\tFCN={:2.6f}\n'.format(self.fcn) +\
+                 '\tXCUT={}\n'.format(self.xcut) +\
+                 '\tYCUT={}\n'.format(self.ycut) +\
+                 '\tJIGTH={}\n'.format(self.jigth) +\
+                 '\tCKOP={:d}\n'.format(self.ckop) +\
+                 '\tUNIQUE={:d}\n'.format(self.unique) +\
+                 '\tMATERIAL="{}"\n'.format(self.material) +\
+                 '\tPUTLST="{}"\n'.format(self.putlst) +\
+                 '\tOPPWKRS={:d}\n'.format(self.oppwkrs) +\
+                 '\tUNICLAMP={:d}\n'.format(self.uniclamp) +\
+                 '\tCHKCOLL={:d}\n'.format(self.chkcoll) +\
+                 '\tWTPIANI={:d}\n'.format(self.wtpiani) +\
+                 '\tCOLLTOOL={:d}\n'.format(self.colltool) +\
+                 '\tCALCEDTH={:d}\n'.format(self.calcedth) +\
+                 '\tENABLELABEL={:d}\n'.format(self.enablelabel) +\
+                 '\tLOCKWASTE={:d}\n'.format(self.lockwaste) +\
+                 '\tLOADEDGEOPT={:d}\n'.format(self.loadedgeopt) +\
+                 '\tITLTYPE={:d}\n'.format(self.itltype) +\
+                 '\tRUNPAV={:d}\n'.format(self.runpav) +\
+                 '\tFLIPEND={:d}\n'.format(self.flipend) +\
+                 '\tENABLEMACHLINKS={:d}\n'.format(self.enablemachlinks) +\
+                 '\tENABLEPURSUITS={:d}\n'.format(self.enablepursuits) +\
+                 '\tENABLEFASTVERTBORINGS={:d}\n'.format(self.enablefastvertborings) +\
+                 '\tFASTVERTBORINGSVALUE={}\n'.format(self.fastvertboringvalue) +\
+                 'END MAINDATA\n'
+
+        return string
+
+
 if __name__=='__main__':
     m=BPPmaindata()
     m.orlst="1,2,3,4"
-    print(m.to_bpp_code())
+    print(m.to_bpp_code150())
+    print(m.to_bpp_code130())
+    print(m.to_vb_script())
+    print(m.to_cid())
+    print(m.to_cix())
